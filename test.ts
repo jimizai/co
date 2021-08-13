3287b15deff6a3602de2038156a027d10a2f3a8d
dyk <359743984@qq.com>
2021-08-13 22:24:35 +0800

    test: add test

--- test.ts
+++ test.ts
@@ -3,13 +3,14 @@ import { co } from ".";
 const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
 function* g() {
+  console.log("g");
   yield sleep(1000);
   return 22;
 }
 
 co.exec(function* () {
-  yield sleep(1000);
   console.log("22");
+  yield sleep(1000);
   const data = yield g;
   console.log("data");
   return data;
